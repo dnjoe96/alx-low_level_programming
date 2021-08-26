f0fbc87d9709b5c280492ebb9a632949ca05a420
dnjoe96 <donjoedbest@gmail.com>
2021-08-26 09:13:27 -0700

    Print size of data types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
  * main - runs command
  * Return: 0
  */
-int main()
+int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
