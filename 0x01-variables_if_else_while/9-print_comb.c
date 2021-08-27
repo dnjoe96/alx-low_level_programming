a9cf0ae12e1601babdeb6afc98bd7ebd8ff9a377
dnjoe96 <donjoedbest@gmail.com>
2021-08-27 20:45:18 +0100

    Print Comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - run code
+ * Return: 0 always
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar('0' + num);
+		
+		if (num < 9) 
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
