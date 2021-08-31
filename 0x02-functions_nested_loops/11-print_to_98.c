aae14e9eff8bd9d46e927baff85137354334e3c5
dnjoe96 <donjoedbest@gmail.com>
2021-08-31 20:45:20 +0100

    Print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,12 +1,35 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Entry point of program
+ * print_to_98 - print time from a given number to 98
  *
- * Return: 0
+ * @n: The given number
+ * Return: void
  */
-int main(void)
+void print_to_98(int n)
 {
-	printf("Holberton\n");
-	return (0);
+	int a, i, j;
+
+	for (a = n; a <= 98; a++)
+	{
+		if (a > 9)
+		{
+			i = a / 10;
+			j = a % 10;
+
+			_putchar('0' + i);
+			_putchar('0' + j);
+		}
+		else
+		{
+			i = a;
+			_putchar('0' + i);
+		}
+
+		if (i != 9 || j != 8)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
 }
--- 0x02-functions_nested_loops/test.c
+++ 0x02-functions_nested_loops/test.c
@@ -6,29 +6,26 @@
 
 
 int main(void) {
-        int a, b, c, d;
+        int n = 2;
+        int i, j;
 
-        for (a = 0; a <= 2; a++) {
+        for (int a = n; a <= 98; a++) {
+            
+            if (a > 9) {
+                i = a / 10;
+                j = a % 10;
                 
-                for (b = 0; b <= 9; b++){
-                    
-                    if (a == 2 && b == 4) {
-                        return (0);
-                    }
-                    
-                    for (c = 0; c <= 5; c++) {
-                        
-                        for (d = 0; d <= 9; d++) {
-                            putchar('0' + a);
-                            putchar('0' + b);
-                            putchar(':');
-                            putchar('0' + c);
-                            putchar('0' + d);
-                            putchar('\n');    
-                        }
-                    }
-                        
-                }
-
+                putchar('0' + i);
+                putchar('0' + j);
+            } else{
+                i = a;
+                putchar('0' + i);
+            }
+            
+            if (i != 9 || j != 8)
+            {
+            putchar(',');
+            putchar(' ');
+            }   
         }
 }
