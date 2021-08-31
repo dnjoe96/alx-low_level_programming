cd412d5e47bf8ff77eaa5467270a63b1f6d3e103
dnjoe96 <donjoedbest@gmail.com>
2021-08-31 14:46:41 +0100

    pull

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * outputs possible combinations of two 2 digits
+ * Return: 0 always
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 1 + a; b <= 9; b++)
+		{
+			putchar('0' + a);
+			putchar('0' + b);
+			if (a != 9 || b != 9)
+			{
+				putchar(' ');
+			}
+		}
+		
+		for (b = 1 + a; b <= 9; b++)
+                {
+                        putchar('0' + a);
+                        if (a != 9 || b != 9)
+                        {
+                                putchar(',');
+                                putchar(' ');
+                        }
+                }
+	}
+
+	putchar('\n');
+	return (0);
+}
