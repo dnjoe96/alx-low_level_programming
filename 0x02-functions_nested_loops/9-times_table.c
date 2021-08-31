5b1072feba99f6556a7f3723a258e9c11dc3f636
dnjoe96 <donjoedbest@gmail.com>
2021-08-31 21:26:36 +0100

    9 Times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,12 +1,42 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Entry point of program
+ * times_table - 9x9 table
  *
- * Return: 0
+ * Return: void
  */
-int main(void)
+void times_table(void)
 {
-	printf("Holberton\n");
-	return (0);
+	int p, i, j;
+
+	for (int n = 0; n <= 9; n++)
+	{
+		for (int m = 0; m <= 9; m++)
+		{
+			p = n * m;
+
+			if (p > 9)
+			{
+				i = p / 10;
+				j = p % 10;
+
+				putchar('0' + i);
+				putchar('0' + j);
+			}
+			else
+			{
+				putchar('0' + p);
+			}
+
+			if (p != 81)
+			{
+				putchar(',');
+				putchar(' ');
+
+				if (p <= 9)
+					putchar(' ');
+			}
+		}
+		putchar('\n');
+	}
 }
