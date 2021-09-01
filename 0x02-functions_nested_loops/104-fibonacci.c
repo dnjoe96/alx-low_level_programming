2b6c0befd656c58dc54f6c77d97f895b75e48cf3
dnjoe96 <donjoedbest@gmail.com>
2021-09-01 20:51:35 +0100

    Last Fib

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - cfinds and prints the first 98 Fibonacci numbers, starting with 1 and 2, 
+ * followed by a new line.umputes first 5o fibonnaci number
+ *
+ * Return: void
+ */
+int main(void)
+{
+	int i;
+	long int num, num1, num2;
+
+	num1 = 1;
+	num2 = 2;
+
+	printf("%ld, ", num1);
+	printf("%ld, ", num2);
+
+	i = 0;
+	while (i < 96)
+	{
+		num = num2;
+		num2 = num1 + num2;
+
+		if (i > 97)
+		{
+			printf("%ld\n", num2);
+			break;
+		}
+		else
+		{
+			printf("%ld, ", num2);
+			num1 = num;
+			i++;
+		}
+	}
+	return (0);
+}
