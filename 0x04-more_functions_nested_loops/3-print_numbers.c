4fd327ddc151463621471081ac2ec5c2c7512414
dnjoe96 <donjoedbest@gmail.com>
2021-09-02 05:51:45 -0700

    Print Numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "holberton.h"
+/**
+ * print_numbers - print number from 0 to 9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar('0' + i);
+	}
+
+	_putchar('\n');
+	return (0);
+}
