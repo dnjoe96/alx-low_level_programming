3634d2c293421f12e2501b6f2b94f32ce828f676
dnjoe96 <donjoedbest@gmail.com>
2021-09-02 03:12:58 -0700

    Check Upper case

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isupper -  a function that checks for uppercase characte.
+ *
+ * @c: letter to be tested
+ * Return: 1 if c is upper case, 0 otherwise
+ */
+int _isupper(int c).
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (0);
+	}
+	else if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: void
+ */
+int main(void)
+{
+	/* print_alphabet();*/
+	print_alphabet_x10();
+
+	return (0);
+}
