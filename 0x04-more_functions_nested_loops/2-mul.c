d3c4d2aab98eaec75082092ea572da4b763bebb0
dnjoe96 <donjoedbest@gmail.com>
2021-09-02 05:23:22 -0700

    Multiply two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include "holberton.h"
+/**
+ * mul - multiply two numbers
+ *
+ * @a: number1
+ * @b: number2
+ * Return: Product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,4 +4,6 @@
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int, int);
+
 #endif
