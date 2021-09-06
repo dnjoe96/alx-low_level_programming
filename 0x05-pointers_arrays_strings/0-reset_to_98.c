b8c2e2c4ca4824d29639d3d83b39ed814f611c2e
dnjoe96 <donjoedbest@gmail.com>
2021-09-06 01:45:41 -0700

    Reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include "holberton.h"
+/**
+ * reset_to_98 - takes a pointer to an int as parameter and updates the value it points to to 98 
+ *
+ * @*n: pointer reference
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include "holberton.h"
+/**
+ * print_diagonal - print n diagonal lines
+ *
+ * @n: number of lines
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 2; j <= i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
