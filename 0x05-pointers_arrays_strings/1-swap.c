c0e5310d1d5f62d616581cfe691c7316c2984848
dnjoe96 <donjoedbest@gmail.com>
2021-09-06 02:28:44 -0700

    Swap Integar

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include "holberton.h"
+/**x
+ * swap_int - a function that swaps the values of two integers
+ *
+ * @*a: pointer to first int
+ * @*b: pointer to second int
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *b;
+	*b = *a;
+	*a = tmp;
+}
