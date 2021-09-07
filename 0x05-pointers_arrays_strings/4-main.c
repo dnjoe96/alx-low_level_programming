157b4304b1fd8a26257836e13eb36dddcf34df96
dnjoe96 <donjoedbest@gmail.com>
2021-09-07 07:21:46 -0700

    Edit print_rev

--- 0x05-pointers_arrays_strings/4-main.c
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -10,6 +10,7 @@ int main(void)
     char *str;
 
     str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+    str = "Holberton!";
     print_rev(str);
     return (0);
 }
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -16,7 +16,7 @@ void print_rev(char *s)
 	while (*(s + len) != '\0')
 		len++;
 
-	for (i = len; i >= 0; i--)
+	for (i = len - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
