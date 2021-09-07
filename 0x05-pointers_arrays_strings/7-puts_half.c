e1b20485a5d8cf5d0eeb9dab9c3e76d472958e48
dnjoe96 <donjoedbest@gmail.com>
2021-09-07 08:34:58 -0700

    Fix Puthalf

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,10 +12,10 @@ void puts_half(char *str)
 
 	len = 0;
 
-	for (i = 0; str[i] != '\0'; i++)
+	while (*(str + len) != '\0')
 		len++;
 
-	for (i = len / 2; i <= len; i++)
+	for (i = len / 2; i < len; i++)
 	{
 		_putchar(str[i]);
 	}
