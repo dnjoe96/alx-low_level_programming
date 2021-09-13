140d05af636e2a21c3a66929e6c0ccd7b3d86e40
dnjoe96 <donjoedbest@gmail.com>
2021-09-13 19:40:59 +0100

    StrStr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -21,7 +21,7 @@ char *_strstr(char *haystack, char *needle)
 		if (*(haystack + a) == *(needle + a))
 		{
 			do {
-				if ((needle + a + 1) == '\0')
+				if (*(needle + a + 1) == '\0')
 					return (haystack);
 
 				a++;
