99917ea74db5a77185b8e5f8821de9e88edccfc8
dnjoe96 <donjoedbest@gmail.com>
2021-09-13 19:37:45 +0100

    StrStr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include "holberton.h"
+/**
+ * _strstr - locate a substring
+ *
+ * @haystack:string
+ * @needle:string
+ * Return:string
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int a;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		a = 0;
+
+		if (*(haystack + a) == *(needle + a))
+		{
+			do {
+				if ((needle + a + 1) == '\0')
+					return (haystack);
+
+				a++;
+
+			} while (*(haystack + a) == *(needle + a));
+		}
+
+		haystack++;
+	}
+
+	return ('\0');
+}
