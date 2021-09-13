423cd65ea5add8b820b021c4e6b3e27de456195f
dnjoe96 <donjoedbest@gmail.com>
2021-09-13 06:43:41 -0700

    Memcopy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "holberton.h"
+/**
+ * _memcpy - a function that copies memory area
+ *
+ * @dest: The destination string
+ * @src: The source string
+ * @n: The number of characters in the string src to copy to dest
+ * Return: A pointer to the array dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		*(src + i) = *(dest + i)
+	}
+
+	return (dest);
+}
