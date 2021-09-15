fe962f9e22911bccbe6fc27b2727f4afd8297eba
dnjoe96 <donjoedbest@gmail.com>
2021-09-15 20:01:29 +0100

    Strlen with Recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,18 +1,13 @@
 /**
- * _strlen_recursion - Uses recursion to find length of string.
- * @s: string to count
+ * _strlen_recursion - A function using recursion to find length of 
+ * string.
  *
+ * @s: string input
  * Return: length of string (int)
  */
 int _strlen_recursion(char *s)
 {
-	int len;
-
-	len = 0;
-	if (*s != '\0')
-	{
-		len++;
-		len += _strlen_recursion(s + 1);
-	}
-	return (len);
+	if (*s == '\0')
+		return (0);
+	return _strlen_recursion(s + 1);
 }
