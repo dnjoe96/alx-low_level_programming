5415f54b79541aec05b2b4f801f730fcbcf5222b
dnjoe96 <donjoedbest@gmail.com>
2021-09-15 20:57:07 +0100

    Power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -7,13 +7,13 @@
  */
 int _pow_recursion(int x, int y)
 {
-	if (y < 0)
-	{
-		return (-1);
-	}
-	else if (y == 0)
-	{
+	if (x == 0)
+		return (0);
+	if (y == 0)
 		return (1);
-	}
+	if (y < 0)
+		return (-1);
+	if (y == 1)
+		return (x);
 	return (x * _pow_recursion(x, y - 1));
 }
