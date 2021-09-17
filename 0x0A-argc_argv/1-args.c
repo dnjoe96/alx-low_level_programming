fc59a4a9667cfc8393937906b53c79541a913187
dnjoe96 <donjoedbest@gmail.com>
2021-09-17 02:29:22 -0700

    Print Args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: The length of the commandline input array
+ * @argv: The input array
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
