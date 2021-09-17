6175524b0d56aba63a9409bbb30554b7b06a6e96
dnjoe96 <donjoedbest@gmail.com>
2021-09-17 02:13:48 -0700

    Whatsmy name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * @argc: The length of the commandline input array
+ * @argv: The input array
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
