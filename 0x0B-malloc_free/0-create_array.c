b20f80f9aeb8bf8613c6dd5ac1f843f37617e980
dnjoe96 <donjoedbest@gmail.com>
2021-09-21 21:49:55 +0100

    Fix create array code

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,12 +15,13 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (size == 0)
-		return ('\0');
+		return (NULL);
 
-	ptr = malloc(sizeof(char) * size);
+	ptr = (char *)malloc(sizeof(char) * size);
 
 	for (i = 0; i < size; i++)
 		ptr[i] = c;
+	ptr[i] = '\0';
 
 	return (ptr);
 }
