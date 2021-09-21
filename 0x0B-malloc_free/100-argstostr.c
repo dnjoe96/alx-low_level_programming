0bc5c7ae7cc4ea8c6055c219e34c16040f5fb8f1
dnjoe96 <donjoedbest@gmail.com>
2021-09-21 20:05:46 +0100

    Arguments To String

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -9,5 +9,33 @@
  */
 char *argstostr(int ac, char **av)
 {
-	return (ac);
+	char *strDup;
+	int i, j, k, size;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	size = 0;
+	/* count the number of chars in each string */
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			size++;
+		size++;
+	}
+	size++;
+
+	strDup = malloc(sizeof(char) * size);
+	if (strDup == NULL)
+		return (NULL);
+	k = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			strDup[k++] = av[i][j];
+		}
+		strDup[k++] = '\n';
+	}
+	strDup[k] = '\0';
+	return (strDup);
 }
