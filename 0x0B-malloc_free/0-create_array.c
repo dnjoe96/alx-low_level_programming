cc263dc71ac53043d58eb39848b2904a0242b8f2
dnjoe96 <donjoedbest@gmail.com>
2021-09-21 05:44:45 -0700

    create Array

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * create_array - a function that creates an array of chars, and
+ * initializes it with a specific char
+ *
+ * @size: size of array
+ * @c: character input
+ * Return: void
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ptr;
+	unsigned int i;
+
+	if (size == 0)
+		return ('\0');
+
+	ptr = malloc(sizeof(char) * size);
+
+	for (i = 0; i < size; i++)
+		ptr[i] = c;
+
+	return (ptr);
+}
--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -1,15 +1,12 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-int _putchar(char c);
-char *_memset(char *s, char b, unsigned int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *_strchr(char *s, char c);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strstr(char *haystack, char *needle);
-void print_chessboard(char (*a)[8]);
-void print_diagsums(int *a, int size);
-void set_string(char **s, char *to);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
 
 #endif
