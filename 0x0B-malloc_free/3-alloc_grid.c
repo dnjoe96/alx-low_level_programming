ae0df741d772140ff0312f293f35e4d829b27774
dnjoe96 <donjoedbest@gmail.com>
2021-09-22 06:07:30 -0700

    Array Allocation

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,14 +17,14 @@ int **alloc_grid(int width, int height)
 	if (width < 1 || height < 1)
 		return (NULL);
 
-	arr = (int **)malloc(sizeof(int *) * width);
+	arr = malloc(sizeof(int *) * width);
 
 	if (arr == NULL)
 		return (NULL);
 
 	for (i = 0; i < width; i++)
 	{
-		*(arr + i) = (int *)malloc(sizeof(int) * height);
+		*(arr + i) = malloc(sizeof(int) * height);
 
 		for (j = 0; j < height; j++)
 		{
