ba57f89dd34f42ecf739a3beb47a5b5fe464481e
dnjoe96 <donjoedbest@gmail.com>
2021-09-22 02:34:52 -0700

    String Duplication

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,6 +1,23 @@
 #include "main.h"
 #include "holberton.h"
 /**
+ * _strlen - get the length of a string
+ *
+ * @s: string input
+ * Return: Length in int
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
+}
+/**
  * _strdup -  a function that returns a pointer to a newly allocated space
  * in memory, which contains a copy of the string given as a parameter
  *
@@ -9,5 +26,19 @@
  */
 char *_strdup(char *str)
 {
-	return (str);
+	char *ptr;
+	unsigned int i, size;
+
+	if (str == NULL)
+		return ('\0');
+
+	size = _strlen(str);
+
+	ptr = (char *)malloc(sizeof(char) * size);
+
+	for (i = 0; i < size; i++)
+		ptr[i] = str[i];
+	ptr[i] = '\0';
+
+	return (ptr);
 }
