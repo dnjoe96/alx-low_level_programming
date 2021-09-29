10242101b1fa1eac38308775bec6f8aae838b348
dnjoe96 <donjoedbest@gmail.com>
2021-09-29 22:19:27 +0100

    Action on Array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * array_iterator - excute a function on elements of an array
+ * 
+ * @array: array
+ * @size: size of array
+ * @action: function to be excuted
+ * Return:void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array != NULL && action != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			action(*(array + i));
+		}
+	}	
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -4,4 +4,7 @@
 int _putchar(char);
 void print_name(char *name, void (*f)(char *));
 
+typedef long unsigned int size_t;
+void array_iterator(int *array, size_t size, void (*action)(int));
+
 #endif
