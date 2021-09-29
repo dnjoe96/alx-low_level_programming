c15ef5184604bff698ce6e1bd9c2666438b12a1a
dnjoe96 <donjoedbest@gmail.com>
2021-09-29 23:38:55 +0100

    Compare

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - Search for an integer
+ * @array:int
+ * @size:int
+ * @cmp:function
+ * Return:index of the integer searched for
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, a;
+
+	if (size <= 0)
+		return (-1);
+
+	if (size && cmp != NULL && array != NULL)
+	{
+		for (i = 0; i < size; i++)
+		{
+			a = cmp(array[i]);
+			if (a != 0)
+				return (i);
+		}
+	}
+	return (-1);
+}
