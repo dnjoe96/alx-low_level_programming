219e6921486fa1acb88e844ab7c2dde3b125be1a
dnjoe96 <donjoedbest@gmail.com>
2021-09-29 20:16:44 +0100

    Start New Project

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+/**
+ * print_name - Prints name
+ *
+ * @name: Name string
+ * @f: point to function
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,7 @@
+#ifndef FUNC_H
+#define FUNC_H
+
+int _putchar(char);
+void print_name(char *name, void (*f)(char *));
+
+#endif
