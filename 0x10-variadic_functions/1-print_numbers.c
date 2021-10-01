175e3fc79b74cd4d6f1fe68ebc971ef54d433a42
dnjoe96 <donjoedbest@gmail.com>
2021-10-01 07:14:53 +0100

    Print Numbers with separator

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - A function that prints numbers
+ *
+ * @separator: The number seperator
+ * @n: The number of number
+ * Return: Void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list nums;
+
+	va_start(nums, n);
+
+	if (n != 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			if (separator != NULL && i > 0)
+				printf("%s", separator);
+
+			printf("%d", va_arg(nums, int));
+		}
+		va_end(nums);
+		printf("\n");
+	}
+}
