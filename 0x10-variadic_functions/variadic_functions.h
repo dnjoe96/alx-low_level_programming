40fd3b4dab1452169bf6038614ec67537122675d
dnjoe96 <donjoedbest@gmail.com>
2021-09-30 21:54:35 -0700

    Variadic Function Project Created

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,11 @@
+#ifndef FUNC_H
+#define FUNC_H
+#include <stdlib.h>
+
+int _putchar(char);
+void print_name(char *name, void (*f)(char *));
+
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
