a0c45e3f78709354659bf1c007be61959dafbb8c
dnjoe96 <donjoedbest@gmail.com>
2021-10-11 09:16:22 -0700

    First commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * listint_len - get the number of elements in a linked list
+ * print_listint - Print the contact of a linked list
  *
  * @h: Pointer to the head of the list
  * Return: Number of nodes
  */
-size_t listint_len(const listint_t *h)
+size_t print_listint(const listint_t *h)
 {
 	int i;
 	listint_t *new;
@@ -19,6 +19,7 @@ size_t listint_len(const listint_t *h)
 
 	for (i = 0; node != NULL; i++)
 	{
+		printf("%d\n", node->n);
 		node = node->next;
 	}
 	free(new);
