d06ed2054b39ebfb1d30119a226aebc83cc080ce
dnjoe96 <donjoedbest@gmail.com>
2021-10-12 22:57:01 +0100

    Add int to a given position

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - Delete a node at a position
+ *
+ * @head: The head of the list
+ * @index: the nth index
+ * Return: Returns 1 if successful otherwise-1
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *node, *last;
+
+	if (head == NULL)
+		return (-1);
+
+	node = *head;
+
+	for (i = 0; node; i++)
+	{
+		if (i == index)
+		{
+			last->next = node->next;
+			free(node);
+
+			return (1);
+		}
+
+		last = node;
+		node = node->next;
+	}
+
+	return (-1);
+}
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -29,7 +29,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (i == idx)
 		{
 			new->n = n;
-			new->next = node->next;
+			new->next = node;
 			last->next = new;
 
 			return (new);
