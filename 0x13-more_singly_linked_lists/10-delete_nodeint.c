72615aa1f141d0f494959559a7efca292449ffb2
dnjoe96 <donjoedbest@gmail.com>
2021-10-13 07:36:02 +0100

    Delete node

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,16 +12,30 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int i;
 	listint_t *node, *last;
 
-	if (head == NULL)
+	if (*head == NULL)
 		return (-1);
 
 	node = *head;
+	
+	if (node->next == NULL && index == 0)
+	{
+		*head = NULL;
+		return (1);
+	}
+
+	if (index == 0)
+	{
+		*head = node->next;
+		free(node);
+		return (1);
+	}
 
 	for (i = 0; node; i++)
 	{
 		if (i == index)
 		{
 			last->next = node->next;
+
 			free(node);
 
 			return (1);
