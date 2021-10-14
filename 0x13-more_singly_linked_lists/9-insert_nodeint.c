69dfdd34062e953f7ba3fe86be4cc4c7c9708ab9
dnjoe96 <donjoedbest@gmail.com>
2021-10-14 04:47:07 +0100

    Fix issue with Insert node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,7 +36,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			if (idx != 0)
 				last->next = new;
 			else
-				node->next = NULL;
+				*head = new;
 
 			return (new);
 		}
