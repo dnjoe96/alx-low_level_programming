d2186204baf9a8004ec3931fb5b79963e8be740f
dnjoe96 <donjoedbest@gmail.com>
2021-10-14 04:53:24 +0100

    Fix issue with Insert node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,7 +17,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (n < 0)
 		return (NULL);
 
-	if (*head == NULL)
+	if (*head == NULL && head == NULL)
 		return (NULL);
 
 	new = malloc(sizeof(listint_t));
