ad9f4cae9ea9a516613fe97f773293ec18823677
dnjoe96 <donjoedbest@gmail.com>
2021-11-26 04:46:33 -0800

    6-sum_dlistint.c

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,1 +1,23 @@
 #include "lists.h"
+
+/**
+ * sum_dlistint -  a function that returns the sum of all the
+ * data (n) of a dlistint_t linked list
+ *
+ * @head: The head node of the list
+ * Return: the sum of the nodes
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *h;
+	int sum = 0;
+
+	h = head;
+
+	while (h != NULL)
+	{
+		sum = sum + h->n;
+		h = h->next;
+	}
+	return (sum);
+}
