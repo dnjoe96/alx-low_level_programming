55aa4edbbf4f8b4d446adbde6a68310dc1829fa7
dnjoe96 <donjoedbest@gmail.com>
2021-12-18 23:08:20 +0100

    Get ad Set value in hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,1 +1,69 @@
 #include "hash_tables.h"
+
+/**
+ * insert - A function to insert value at a hash table index
+ *
+ * @array: Pointer to hash table array
+ * @index: Index to be inserted
+ * @node: The pointer to node to be inserted
+ * Return: 1
+ */
+int insert(hash_node_t **array, unsigned long index, hash_node_t *node)
+{
+	hash_node_t *head;
+
+	if (array[index] == NULL)
+	{
+		array[index] = node;
+		return (1);
+	}
+	else
+	{
+		head = array[index];
+
+		if (head->next == NULL)
+		{
+			head->next = node;
+			return (1);
+		}
+
+		while (head->next != NULL)
+		{
+			head = head->next;
+		}
+		head->next = node;
+
+		return (1);
+	}
+}
+
+/**
+ * hash_table_set - add an element to the hash table
+ *
+ * @ht: hash table to add or update
+ * @key: The key
+ * @value: The value
+ * Return: 1 if successful, 0 otherwise
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *node;
+	unsigned long int index;
+	unsigned long int size = ht->size;
+	hash_node_t **array = ht->array;
+
+	node = malloc(sizeof(hash_node_t));
+
+	if (node == NULL)
+		return (0);
+
+	node->key = (char *)key;
+	node->value = (char *)value;
+	node->next = NULL;
+
+	index = key_index((const unsigned char *)key, size);
+
+	insert(array, index, node);
+
+	return (1);
+}
--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,1 +1,58 @@
 #include "hash_tables.h"
+
+/**
+ * find - A function to find a key in a hash table and return node
+ *
+ * @array: Pointer to hash table array
+ * @index: Index to be inserted
+ * @key: key
+ *
+ * Return: node
+ */
+hash_node_t *find(hash_node_t **array, unsigned long index, const char *key)
+{
+	if (array[index] == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		hash_node_t *head;
+
+		head = array[index];
+
+		while (head != NULL)
+		{
+			if (head->key == key)
+				return (head);
+
+			head = head->next;
+		}
+
+		return (NULL);
+	}
+}
+
+/**
+ * hash_table_get - get an element in the hash table
+ *
+ * @ht: hash table to add or update
+ * @key: The key
+ * Return: value if successful, NULL if key cannot be found
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *node;
+	unsigned long int index;
+	unsigned long int size = ht->size;
+	hash_node_t **array = ht->array;
+
+	index = key_index((const unsigned char *)key, size);
+
+	node = find(array, index, key);
+
+	if (node == NULL)
+		return (NULL);
+
+	return (node->value);
+}
