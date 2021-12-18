7a59e618d0d1fe427e2df97d659ff014273287c0
dnjoe96 <donjoedbest@gmail.com>
2021-12-18 12:56:49 +0100

    Gen Key index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,1 +1,17 @@
 #include "hash_tables.h"
+
+/**
+ * key_index - Get and returns the key index for the hash table
+ *
+ * @key: The key
+ * @size: The size of the array table
+ * Return: The Index, btw 0 to size_of_array - 1
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index;
+
+	index = hash_djb2(key) % size;
+
+	return (index);
+}
